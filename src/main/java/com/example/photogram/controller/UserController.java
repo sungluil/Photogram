8fd51426ec14c973d148e76ca0fc37f8c88ec992
sungluil <josungluil@naver.com>
2021-06-19 20:22:47 +0900

    AuthenticationPrincipal 추가 내정보 연동

--- src/main/java/com/example/photogram/controller/UserController.java
+++ src/main/java/com/example/photogram/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.photogram.controller;
 
+import com.example.photogram.config.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,10 @@ public class UserController {
 	}
 
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+		System.out.println("세션 정보 : "+principalDetails.getUser());
+
 		return "user/update";
 	}
 
