75c903532a8e7e9c19e037cdb39b1f74e1d29346
sungluil <josungluil@naver.com>
2021-07-20 03:08:11 +0900

    수정6

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,6 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/appliaction.properties \
-        $REPOSITORY/$JAR_NAME nohup.out 2>&1 &
+nohup java -jar $REPOSITORY/$JAR_NAME nohup.out 2>&1 &
